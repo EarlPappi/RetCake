9839204321f1069430ec42eeb4886512a2dbc6e3
earlpappi <earlpappi@gmail.com>
2022-07-30 01:24:12 +0100

    tried btn component -- not working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './Components/Directory/Home/Home';
 import { ContextProvider } from './Context/AuthContext';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeaderNav from './Components/Header/HeaderNav/HeaderNav';
+import Signup from './Components/Directory/Auth/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="signup" element={ <Signup/> } />
 
         </Routes>
       </BrowserRouter>
--- src/Components/TinyComp/Btn/Btn.jsx
+++ src/Components/TinyComp/Btn/Btn.jsx
@@ -1,8 +1,20 @@
-import { Link } from "react-router";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import classes from "./Btn.module.css"
 
-const Btn = () => {
+const Btn = ({ children, Style, To }) => {
+    const [alt, setAlt] = useState();
+
+    { Style ? setAlt({
+        color: "#A01114",
+        backgroundColor: "white",
+        border: "1px solid #A01114"
+        
+    }) : setAlt()}
+    
+
     return ( 
-        <Link></Link>
+        <Link to={ To } className={ classes.linkBtn } style={ alt }>{ children }</Link>
      );
 }
  
